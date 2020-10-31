3980144a867054c61626dbf19bb18f846d1bfc93
Omri Arad <omriarad3@gmail.com>
2020-10-31 17:24:41 +0200

    AST: add renaming skeleton
    Signed-off-by: Omri Arad <omriarad3@gmail.com>

---
+++ src/ast/AstRenamingVisitor.java
@@ -0,0 +1,196 @@
+package ast;
+import java.util.*;
+
+public class AstRenamingVisitor implements Visitor {
+    private String currentClass;
+    private Set<String> susClasses;
+    private Map<String,String> scopeToType;
+    private Map<String,String> fieldToType;
+
+    private void visitBinaryExpr(BinaryExpr e, String infixSymbol) {
+        e.e1().accept(this);
+        e.e2().accept(this);
+    }
+
+    @Override
+    public void visit(Program program) {
+        program.mainClass().accept(this);
+        for (ClassDecl classdecl : program.classDecls()) {
+            classdecl.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(ClassDecl classDecl) {
+        for (var fieldDecl : classDecl.fields()) {
+            fieldDecl.accept(this);
+        }
+
+        for (var methodDecl : classDecl.methoddecls()) {
+            methodDecl.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(MainClass mainClass) {
+        mainClass.mainStatement().accept(this);
+    }
+
+    @Override
+    public void visit(MethodDecl methodDecl) {
+        methodDecl.returnType().accept(this);
+
+        for (var formal : methodDecl.formals()) {
+            formal.accept(this);
+        }
+
+        for (var varDecl : methodDecl.vardecls()) {
+            varDecl.accept(this);
+        }
+        for (var stmt : methodDecl.body()) {
+            stmt.accept(this);
+        }
+
+        methodDecl.ret().accept(this);
+    }
+
+    @Override
+    public void visit(FormalArg formalArg) {
+        formalArg.type().accept(this);
+    }
+
+    @Override
+    public void visit(VarDecl varDecl) {
+        varDecl.type().accept(this);
+    }
+
+    @Override
+    public void visit(BlockStatement blockStatement) {
+        for (var s : blockStatement.statements()) {
+            s.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(IfStatement ifStatement) {
+        ifStatement.cond().accept(this);
+        ifStatement.thencase().accept(this);
+        ifStatement.elsecase().accept(this);
+    }
+
+    @Override
+    public void visit(WhileStatement whileStatement) {
+        whileStatement.cond().accept(this);
+        whileStatement.body().accept(this);
+    }
+
+    @Override
+    public void visit(SysoutStatement sysoutStatement) {
+        sysoutStatement.arg().accept(this);
+    }
+
+    @Override
+    public void visit(AssignStatement assignStatement) {
+        assignStatement.rv().accept(this);
+    }
+
+    @Override
+    public void visit(AssignArrayStatement assignArrayStatement) {
+        assignArrayStatement.index().accept(this);
+        assignArrayStatement.rv().accept(this);
+    }
+
+    @Override
+    public void visit(AndExpr e) {
+        visitBinaryExpr(e, "&&");
+    }
+
+    @Override
+    public void visit(LtExpr e) {
+        visitBinaryExpr(e, "<");;
+    }
+
+    @Override
+    public void visit(AddExpr e) {
+        visitBinaryExpr(e, "+");;
+    }
+
+    @Override
+    public void visit(SubtractExpr e) {
+        visitBinaryExpr(e, "-");
+    }
+
+    @Override
+    public void visit(MultExpr e) {
+        visitBinaryExpr(e, "*");
+    }
+
+    @Override
+    public void visit(ArrayAccessExpr e) {
+        e.arrayExpr().accept(this);
+        e.indexExpr().accept(this);
+    }
+
+    @Override
+    public void visit(ArrayLengthExpr e) {
+        e.arrayExpr().accept(this);
+    }
+
+    @Override
+    public void visit(MethodCallExpr e) {
+        e.ownerExpr().accept(this);
+
+        for (Expr arg : e.actuals()) {
+            arg.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(IntegerLiteralExpr e) {
+    }
+
+    @Override
+    public void visit(TrueExpr e) {
+    }
+
+    @Override
+    public void visit(FalseExpr e) {
+    }
+
+    @Override
+    public void visit(IdentifierExpr e) {
+    }
+
+    public void visit(ThisExpr e) {
+    }
+
+    @Override
+    public void visit(NewIntArrayExpr e) {
+        e.lengthExpr().accept(this);
+    }
+
+    @Override
+    public void visit(NewObjectExpr e) {
+    }
+
+    @Override
+    public void visit(NotExpr e) {
+        e.e().accept(this);
+    }
+
+    @Override
+    public void visit(IntAstType t) {
+    }
+
+    @Override
+    public void visit(BoolAstType t) {
+    }
+
+    @Override
+    public void visit(IntArrayAstType t) {
+    }
+
+    @Override
+    public void visit(RefType t) {
+    }
+}
