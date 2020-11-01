05b893678a7c5fe128a7b3cdf84b38a8f9639d46
Omri Arad <omriarad3@gmail.com>
2020-11-01 20:58:48 +0200

    examples: fix ref id
    Signed-off-by: Omri Arad <omriarad3@gmail.com>

--- examples/ex1/hierarchy_1.java
+++ examples/ex1/hierarchy_1.java
@@ -1,8 +1,8 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println(new PremuimUser().getName());
+        System.out.println(new PremuimUser().getAge());
     }
-} 
+}
 
 class User {
     boolean active;
