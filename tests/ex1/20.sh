73437105d85c4ab2b0ae6e5ce8ad158cd23040c4
Guy Haviv <theguyhaviv@gmail.com>
2020-11-14 16:14:27 +0200

    new test 20: method masks field over same name

---
+++ tests/ex1/20.sh
@@ -0,0 +1,2 @@
+# variable renaming -> changing field name w/ same name as a later declared method
+java -jar ../../mjavac.jar unmarshal rename var mask 8 field ../../examples/ex1/method_over_field.java.xml result.xml
