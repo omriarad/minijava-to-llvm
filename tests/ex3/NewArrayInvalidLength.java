6fc94b1c75786e4f910581c87b534dfbdf4f4f9a
Guy Haviv <theguyhaviv@gmail.com>
2020-12-21 15:31:17 +0200

    test/ex3: checks 22-23 and new int length check

---
+++ tests/ex3/NewArrayInvalidLength.java
@@ -0,0 +1,16 @@
+class Main{
+    public static void main(String[] args){
+        System.out.println(new A().test(4));
+    }
+}
+
+class A {
+    boolean bool;
+    int test(int num){
+        int[] arr;
+        int res;
+        arr = new int[bool];
+        arr[3] = 9;
+        return 5;
+    }
+}
