730b8a5129a6baf15f3ea5e9018560d2583762e1
yotamfe <yotam.feldman@gmail.com>
2020-12-30 19:43:56 +0200

    Examples for ex4

---
+++ examples/ex4/Valid.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] a) { 
+        System.out.println(3);
+    }
+}
+    
+    class Tree {
+        Tree left;
+        // Tree right;
+
+        public Tree getLeft() {
+            return left;
+        }
+
+        public int fun() {
+            int x;
+            x = 1;
+            /* x = 0; ** */
+        	return x;
+    	}
+}
